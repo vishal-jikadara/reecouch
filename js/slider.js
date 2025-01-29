74f37e7319e008f9cb58a0a2a6bca3182342ce71
vishal-jikadara <vishaljikadara28@gmail.com>
2025-01-29 12:40:35 +0530

    one_sectionadd

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+// var mySwiper = new Swiper ('.swiper-container', {
+//     direction: 'vertical',
+//     effect: 'slide',
+//     slidesPerView: 1,
+//     loop: true,
+//     autoplay: {
+//         delay: 2000,
+//         reverseDirection: true,
+//         disableOnInteraction: false,
+//     },
+// })
